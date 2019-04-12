138a1383bbb5fe931d4b7006a5be738a1de7b6bc
Ivanov Mikhail <tridetch@gmail.com>
2019-04-12 13:59:20 +0300

    work request report screen

--- app/src/main/java/ru/climatlab/service/ui/requestReport/RequestReportActivity.kt
+++ app/src/main/java/ru/climatlab/service/ui/requestReport/RequestReportActivity.kt
@@ -1,18 +1,33 @@
 package ru.climatlab.service.ui.requestReport
 
+import android.app.Activity
 import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.arellomobile.mvp.presenter.InjectPresenter
+import com.bumptech.glide.Glide
+import com.github.dhaval2404.imagepicker.ImagePicker
 import kotlinx.android.synthetic.main.activity_request_report.*
 import ru.climatlab.service.R
 import ru.climatlab.service.ui.BaseActivity
 import ru.climatlab.service.ui.requestDetailsInfo.RequestDetailsActivity
+import java.io.ByteArrayOutputStream
+import java.io.File
+
 
 class RequestReportActivity : BaseActivity(), RequestReportView {
     companion object {
         const val EXTRA_KEY_REQUEST_ID = "extra_key_request_id"
     }
+
+    private val REQUEST_CODE_BOILER_PHOTO = 1
+    private val REQUEST_CODE_RESULT_PHOTO = 2
 
     @InjectPresenter
     lateinit var presenter: RequestReportPresenter
@@ -23,7 +38,101 @@ class RequestReportActivity : BaseActivity(), RequestReportView {
 
         presenter.onAttach(intent.getStringExtra(RequestDetailsActivity.EXTRA_KEY_REQUEST_ID))
 
-        confirmButton.setOnClickListener { presenter.onReportConfirm() }
+        boilerPhotoCard.setOnClickListener {
+            ImagePicker.with(this)
+                .cameraOnly()
+                .maxResultSize(480, 720)
+                .start(REQUEST_CODE_BOILER_PHOTO)
+        }
+        resultPhotoCard.setOnClickListener {
+            ImagePicker.with(this)
+                .cameraOnly()
+                .maxResultSize(480, 720)
+                .start(REQUEST_CODE_RESULT_PHOTO)
+        }
+
+        confirmButton.setOnClickListener {
+            presenter.onReportConfirm(
+                model = modelEditText.text.toString(),
+                brand = brandEditText.text.toString(),
+                serialNumber = serialNumberEditText.text.toString(),
+                voltage = voltageEditText.text.toString(),
+                grounding = groundingCheckBox.isChecked,
+                stabilizer = stabilizerCheckBox.isChecked,
+                dielectricCoupling = dielectricCouplingCheckBox.isChecked,
+                inletGasPressure = inletGasPressureEditText.text.toString(),
+                minimumGasOnTheBoiler = minGasEditText.text.toString(),
+                maximumGasOnTheBoiler = maxGasEditText.text.toString(),
+                co = coEditText.text.toString(),
+                co2 = co2EditText.text.toString(),
+                recommendations = recommendationsInputLayout.editText!!.text.toString(),
+                amountToPay = summaryToPayEditText.text.toString(),
+                amountForTheRoad = summaryForRoadEditText.text.toString(),
+                amountOfPart = summaryForPartsEditText.text.toString()
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_BOILER_PHOTO -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    //Image Uri will not be null for RESULT_OK
+                    val fileUri = data?.data
+                    //You can get File object from intent
+                    val file: File = ImagePicker.getFile(data)!!
+                    //You can also get File Path from intent
+                    val filePath: String = ImagePicker.getFilePath(data)!!
+
+                    val bm = BitmapFactory.decodeFile(ImagePicker.getFilePath(data))
+                    val baos = ByteArrayOutputStream()
+                    bm.compress(Bitmap.CompressFormat.JPEG, 80, baos) //bm is the bitmap object
+                    val b = baos.toByteArray()
+                    val encodedImage = Base64.encodeToString(b, Base64.DEFAULT)
+                    presenter.onBoilerPhotoTaken(encodedImage!!)
+
+                    boilerPhotoImageView.visibility = View.VISIBLE
+                    boilerPhotoPlaceholder.visibility = View.GONE
+
+                    Glide.with(this)
+                        .load(fileUri)
+                        .into(boilerPhotoImageView)
+                } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                    Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                }
+            }
+            REQUEST_CODE_RESULT_PHOTO->{
+                if (resultCode == Activity.RESULT_OK) {
+                    //Image Uri will not be null for RESULT_OK
+                    val fileUri = data?.data
+                    //You can get File object from intent
+                    val file: File = ImagePicker.getFile(data)!!
+                    //You can also get File Path from intent
+                    val filePath: String = ImagePicker.getFilePath(data)!!
+
+                    val bm = BitmapFactory.decodeFile(ImagePicker.getFilePath(data))
+                    val baos = ByteArrayOutputStream()
+                    bm.compress(Bitmap.CompressFormat.JPEG, 80, baos) //bm is the bitmap object
+                    val b = baos.toByteArray()
+                    val encodedImage = Base64.encodeToString(b, Base64.DEFAULT)
+                    presenter.onResultPhotoTaken(encodedImage!!)
+
+                    resultPhotoImageView.visibility = View.VISIBLE
+                    resultPhotoPlaceholder.visibility = View.GONE
+
+                    Glide.with(this)
+                        .load(fileUri)
+                        .into(resultPhotoImageView)
+                } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                    Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun showRequestNotFoundError() {
--- app/src/main/java/ru/climatlab/service/ui/requestReport/RequestReportPresenter.kt
+++ app/src/main/java/ru/climatlab/service/ui/requestReport/RequestReportPresenter.kt
@@ -8,8 +8,8 @@ import ru.climatlab.service.ui.BasePresenter
 class RequestReportPresenter: BasePresenter<RequestReportView>() {
 
     private lateinit var request: RequestModel
+    private var requestReport = RequestReport()
 
-    private var requesrReport = RequestReport()
     fun onAttach(requestId: String?) {
         val cachedRequest = ClimatLabRepositoryProvider.instance.getRequest(requestId)
         if (cachedRequest == null) {
@@ -18,6 +18,55 @@ class RequestReportPresenter: BasePresenter<RequestReportView>() {
             request = cachedRequest
         }
 
+    }
+
+    fun onReportConfirm(model: String = requestReport.model,
+                        brand: String = requestReport.brand,
+                        serialNumber: String = requestReport.serialNumber,
+                        presenceOfPickup: String = requestReport.presenceOfPickup,
+                        voltage: String = requestReport.voltage,
+                        grounding: Boolean = requestReport.grounding,
+                        stabilizer: Boolean = requestReport.stabilizer,
+                        dielectricCoupling: Boolean = requestReport.dielectricCoupling,
+                        inletGasPressure: String = requestReport.inletGasPressure,
+                        minimumGasOnTheBoiler: String = requestReport.minimumGasOnTheBoiler,
+                        maximumGasOnTheBoiler: String = requestReport.maximumGasOnTheBoiler,
+                        co: String = requestReport.co,
+                        co2: String = requestReport.co2,
+                        recommendations: String = requestReport.recommendations,
+                        amountToPay: String = requestReport.amountToPay,
+                        amountForTheRoad: String = requestReport.amountForTheRoad,
+                        amountOfPart: String = requestReport.amountOfPart,
+                        boilerPhoto: String = requestReport.boilerPhoto,
+                        resultPhoto: String = requestReport.resultPhoto) {
+        requestReport = requestReport.copy(
+            model = model,
+            brand = brand,
+            serialNumber = serialNumber,
+            presenceOfPickup = presenceOfPickup,
+            voltage = voltage,
+            grounding = grounding,
+            stabilizer = stabilizer,
+            dielectricCoupling = dielectricCoupling,
+            inletGasPressure = inletGasPressure,
+            minimumGasOnTheBoiler = minimumGasOnTheBoiler,
+            maximumGasOnTheBoiler = maximumGasOnTheBoiler,
+            co = co,
+            co2 = co2,
+            recommendations = recommendations,
+            amountToPay = amountToPay,
+            amountForTheRoad = amountForTheRoad,
+            amountOfPart = amountOfPart,
+            boilerPhoto = boilerPhoto,
+            resultPhoto = resultPhoto
+        )
+    }
+
+    fun onBoilerPhotoTaken(boilerPhotoBase64: String) {
+        requestReport = requestReport.copy(boilerPhoto = boilerPhotoBase64)
+    }
+    fun onResultPhotoTaken(resultPhotoBase64: String) {
+        requestReport = requestReport.copy(resultPhoto = resultPhotoBase64)
     }
 
 
